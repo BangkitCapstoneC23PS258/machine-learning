2759ec078f0caa656a14b8f13d045955b6542b53
Yusron Izza Faradisa <yusronizzafaradisa@gmail.com>
2023-06-09 10:29:24 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+class_names = {'ayam_bakar': 0,
+ 'ayam_betutu': 1,
+ 'ayam_pop': 2,
+ 'bakso': 3,
+ 'bakwan': 4,
+ 'batagor': 5,
+ 'beberuk_terong': 6,
+ 'capcay': 7,
+ 'coto_makasar': 8,
+ 'dendeng_batokok': 9,
+ 'gado_gado': 10,
+ 'gudeg': 11,
+ 'gulai_ikan': 12,
+ 'gulai_tambusu': 13,
+ 'gulai_tunjang': 14,
+ 'kerak_telur': 15,
+ 'kue_dadar_gulung': 16,
+ 'mie_aceh': 17,
+ 'nasi_kuning': 18,
+ 'papeda': 19,
+ 'pempek': 20,
+ 'peuyeum': 21,
+ 'rawon': 22,
+ 'rendang': 23,
+ 'sate': 24,
+ 'sate_lilit': 25,
+ 'soto_banjar': 26,
+ 'soto_lamongan': 27,
+ 'tahu_sumedang': 28}
+
+our_model = load_model('model.h5', compile = False)
+
+def predict_class(model, images):
+  for img in images:
+    img = image.load_img("static/"+img, target_size=(150, 150))
+    img = image.img_to_array(img)                    
+    img = np.expand_dims(img, axis=0)/255                                   
+    pred = model.predict(img)
+    index = np.argmax(pred)
+    pred = list(class_names.keys())[list(class_names.values()).index(index)]
+  return pred
+
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+	return "Open Source API"
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+		
+		images = []
+		for file_name in os.listdir('static'):
+			images.append(file_name)
+		
+		result = predict_class(our_model, images)
+
+	return render_template("index.html", prediction = result, img_path = img_path)
+
+if __name__ =='__main__':
+	app.debug = True
+	app.run(debug = True)
